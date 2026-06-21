170d9653cf5328ff576ef5a4a5d819285d493202
VishakhaChaudhari <vishakhachaudhari04@gmail.com>
2026-06-21 19:48:31 +0530

    Added new options for namskar

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 window.CONFIG = {
   // Replace this with your Firebase web app config from Firebase Console.
   FIREBASE_CONFIG: {
-  apiKey: "AIzaSyAjrcFjwwnolCRIvFMNwviMYhIvuvZ67oU",
-  authDomain: "daily-seva-tracker.firebaseapp.com",
-  projectId: "daily-seva-tracker",
-  storageBucket: "daily-seva-tracker.firebasestorage.app",
-  messagingSenderId: "986908669626",
-  appId: "1:986908669626:web:4e5d0d0bf56af691617483",
-  measurementId: "G-QCFJR9G2TX"
+    apiKey: "AIzaSyAjrcFjwwnolCRIvFMNwviMYhIvuvZ67oU",
+    authDomain: "daily-seva-tracker.firebaseapp.com",
+    projectId: "daily-seva-tracker",
+    storageBucket: "daily-seva-tracker.firebasestorage.app",
+    messagingSenderId: "986908669626",
+    appId: "1:986908669626:web:4e5d0d0bf56af691617483",
+    measurementId: "G-QCFJR9G2TX"
   },
-
 
   ADMINS: ["Narendra Chaudhari", "Vishakha Chaudhari"],
 
@@ -23,6 +22,35 @@ window.CONFIG = {
     "Himanshi Jadhav",
     "Shravani Panmand",
     "Shruti Garud"
+  ],
+
+  POLL_QUESTIONS: [
+    {
+      id: "seva",
+      title: "आजचा प्रश्न",
+      text: "आजची ({date}) श्री स्वामी समर्थ महाराजांची सेवा केली का?",
+      options: [
+        { id: "A", label: "होय ३ अध्याय वाचन व ११ माळी जप केला" },
+        { id: "B", label: "फक्त ३ अध्याय श्री स्वामी चरित्र क्रमशः वाचन केले" },
+        { id: "C", label: "फक्त ११ माळी जप श्री स्वामी समर्थ मंत्रांचा केला" },
+        { id: "D", label: "११ पेक्षा कमी माळी जप श्री स्वामी समर्थ मंत्रांचा केला" },
+        { id: "E", label: "नाही - आज सेवा केली नाही" }
+      ]
+    },
+    {
+      id: "parents_namaskar",
+      title: "आजचा प्रश्न",
+      text: "आज आई वडिलांना नमस्कार केला का ?",
+      options: [
+        { id: "yes", label: "होय" },
+        { id: "no", label: "नाही" }
+      ]
+    }
+  ],
+
+  YES_NO_OPTIONS: [
+    { id: "yes", label: "होय" },
+    { id: "no", label: "नाही" }
   ],
 
   SEVA_QUESTION: "आजची ({date}) श्री स्वामी समर्थ महाराजांची सेवा केली का?",
